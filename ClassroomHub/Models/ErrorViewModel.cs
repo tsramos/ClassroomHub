11ee45b18435375f9f08becf672fbbcb916a4dcb
Thiago <thiago.s.ramos@outlook.com>
2023-05-11 22:14:55 -0300

    Initial commit

---
+++ ClassroomHub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClassroomHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
