9e777aeac90f1ff156431fe3d6d5bb870f837437
Thiago <thiago.s.ramos@outlook.com>
2023-05-17 22:33:32 -0300

    Comitando arquivos não salvos do ultimo commit

--- ClassroomHub.Core/Entities/Activity.cs
+++ ClassroomHub.Core/Entities/Activity.cs
@@ -7,8 +7,5 @@ namespace ClassroomHub.Core.Entities
         public Guid TeacherId { get; set; }
         public Teacher Teacher { get; set; }
         public Guid ModuleId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime DueDate { get; set; }
     }
 }
